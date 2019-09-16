639c5c2547d8ad2dce5a7e9893566a926a14bb5d
ahmedelnawawy <ahmedelnawawy978@gmail.com>
2019-09-16 11:16:54 +0200

    commit

--- LmsaEntitiesDB.cs
+++ LmsaEntitiesDB.cs
@@ -19,6 +19,7 @@ namespace EBEWebForm.Models
     {
         public static string ConvertDate(string date)
         {
+
             //string bbsget;
             string sadater;
             //string stimer;
